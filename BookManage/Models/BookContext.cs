a21469ee55060fa408ab375dce89411944128419
Jackson <deer690535256@gmail.com>
2022-05-20 18:31:03 +1200

    Add project files.

---
+++ BookManage/Models/BookContext.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace BookManage.Models
+{
+    using System;
+    using System.Data.Entity;
+    
+    public partial class BookContext : DbContext
+    {
+        public DbSet<Books> Books { get; set; }
+    }
+}
